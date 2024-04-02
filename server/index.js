57668abde7015c23fe3b3ae368392a43a7f02be4
techDhananjay <dhananjay372001@gmail.com>
2024-04-02 17:13:31 +0530

    dev

--- server/index.js
+++ server/index.js
@@ -3,6 +3,16 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 const mongoose = require('mongoose');
+const { PubSub } = require("graphql-subscriptions")
+const http = require('http')
+
+const { useServer } = require('graphql-ws/lib/use/ws');
+const { WebSocketServer } = require('ws');
+
+
+
+
+
 
 // Connect to MongoDB
 const URL = 'mongodb+srv://dhananjay372001:kVQzFriX8RV856ob@cluster0.giv6mdz.mongodb.net/liveDoData'
@@ -13,6 +23,11 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('Connected to MongoDB');
 });
+
+
+
+
+const pubsub = new PubSub();
 
 // Define MongoDB schema
 const todoSchema = new mongoose.Schema({
@@ -53,12 +68,15 @@ const resolvers = {
         addTodo: async (_, { text }) => {
             const todo = new Todo({ text, completed: false });
             await todo.save();
+            pubsub.publish('TODO_ADDED', { todoAdded: todo });
             return todo;
         },
+
         toggleTodoCompleted: async (_, { id }) => {
             const todo = await Todo.findById(id);
             todo.completed = !todo.completed;
             await todo.save();
+            pubsub.publish('TODO_TOGGLED', { todoToggled: todo });
             return todo;
         }
     },
@@ -72,21 +90,33 @@ const resolvers = {
     }
 };
 
-// Apollo Server setup
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-});
 
 const app = express();
-// server.applyMiddleware({ app });
-
-(async () => {
-    await server.start();
-    server.applyMiddleware({ app });
-})();
-
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req, res }) => ({ req, res, pubsub })
 });
+
+
+server.start().then(res => {
+    server.applyMiddleware({ app });
+
+    const httpServer = http.createServer(app);
+    // Set up WebSocket server
+    const wsServer = new WebSocketServer({
+        server: httpServer,
+        path: '/graphql',
+    });
+
+    useServer({ schema: server.schema }, wsServer);
+
+    httpServer.listen(PORT, () => {
+        console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+        console.log(`Subscriptions ready at ws://localhost:${PORT}/graphql`);
+    });
+});
+
